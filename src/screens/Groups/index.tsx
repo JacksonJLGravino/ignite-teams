e5c65ff95f7d76499dd854889876581a0d93845a
Jackson Gravino <jacksonjlgravino@gmail.com>
2024-04-09 21:50:43 -0300

    part 1

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export function Groups() {
+  return (
+    <View>
+      <Text>Groups</Text>
+      <Text>Groups</Text>
+      <Text>Groups</Text>
+      <Text>Groups</Text>
+      <Text>Groups</Text>
+    </View>
+  );
+}
